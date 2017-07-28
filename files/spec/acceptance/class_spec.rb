cdea56de299f7b92d8f57b9021cc7c4da5914a08
P. Gomersbach <p.gomersbach@rely.nl>
2017-07-28 06:42:47 +0000

    add serverspec test

--- files/spec/acceptance/class_spec.rb
+++ files/spec/acceptance/class_spec.rb
@@ -39,6 +39,11 @@ describe 'profile_elasticsearch class' do
       it { is_expected.to be_installed }
     end
 
+    describe service('kibana') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
     describe port(8080) do
       it { should be_listening }
     end
